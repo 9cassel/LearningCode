ec779e2214bc21ae0c4c563b1707c6b71eb107ef
Cassel Chen <kaonashi552@outlook.com>
2020-08-02 01:41:27 +0800

    On branch master  Your branch is up to date with 'origin/master'.

--- JavaSE/src/com/qifan/javase/Reflect/Teacher.java
+++
@@ -1,7 +0,0 @@
-package com.qifan.javase.Reflect;
-
-public class Teacher {
-    String name;
-    int age;
-    boolean gender;
-}
--- JavaSE/src/com/qifan/javase/test/ReadFileBlockThread.java
+++
@@ -1,7 +0,0 @@
-package com.qifan.javase.test;
-
-public class ReadFileBlockThread extends Thread {
-
-
-
-}
